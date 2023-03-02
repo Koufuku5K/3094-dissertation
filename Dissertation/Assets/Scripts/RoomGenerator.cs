ec5f8065959276c9e0829e6f4d472e3b5eb48961
William Moses <wmoses5k@gmail.com>
2023-03-02 17:41:51 +0000

    Implemented Manhattan Distance

--- Dissertation/Assets/Scripts/RoomGenerator.cs
+++ Dissertation/Assets/Scripts/RoomGenerator.cs
@@ -7,11 +7,9 @@ public class RoomGenerator : MonoBehaviour
     public GameObject roomPrefab;
     public GameObject corridorPrefab;
     public List<GameObject> rooms = new List<GameObject>();
-    private List<GameObject> visited = new List<GameObject>();
-    private List<GameObject> unvisited = new List<GameObject>();
+    //private List<GameObject> visited = new List<GameObject>();
+    //private List<GameObject> unvisited = new List<GameObject>();
 
-    private CheckRoomOverlap checkRoomOverlap;
-   
     // Start is called before the first frame update
     void Start()
     {
@@ -31,16 +29,16 @@ public class RoomGenerator : MonoBehaviour
 
         for (int i = 0; i < numOfRooms; i++)
         {
-            float width = Random.Range(1.0f, 5.0f);
-            float height = Random.Range(1.0f, 5.0f);
-            float length = Random.Range(1.0f, 5.0f);
+            float width = Random.Range(2.0f, 5.0f);
+            float height = Random.Range(2.0f, 5.0f);
+            float length = Random.Range(2.0f, 5.0f);
+
+            // Determine the position of the room
+            float x = Random.Range(-100.0f, 100.0f);
+            float z = Random.Range(-100.0f, 100.0f);
 
             GameObject room = Instantiate(roomPrefab);
             room.transform.localScale = new Vector3(width, height, length);
-
-            // Move the instantiated room to a random position
-            float x = Random.Range(-100.0f, 100.0f);
-            float z = Random.Range(-100.0f, 100.0f);
 
             room.transform.position = new Vector3(x, 0, z);
 
@@ -48,28 +46,118 @@ public class RoomGenerator : MonoBehaviour
         }
 
         ConnectRooms(rooms[0], rooms[1]);
+        ConnectRooms(rooms[1], rooms[2]);
+        ConnectRooms(rooms[2], rooms[3]);
     }
 
     void ConnectRooms(GameObject startingRoom, GameObject destinationRoom)
     {
-        // Calculate the midpoint between the two rooms
+        GameObject startingPoint = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        startingPoint.transform.localScale = new Vector3(1f, 1f, 1f);
+        startingPoint.transform.position = startingRoom.transform.position;
+
+        GameObject target = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        target.transform.localScale = new Vector3(1f, 1f, 1f);
+        target.transform.position = destinationRoom.transform.position;
+
+        /*// Calculate the midpoint between the two rooms
         Vector3 midpoint = (startingRoom.transform.position + destinationRoom.transform.position) / 2f;
 
         // Calculate the distance between the two rooms
         float distance = Vector3.Distance(startingRoom.transform.position, destinationRoom.transform.position);
 
-        // Create a corridor as a thin, rectangular cube
+        // Create a corridor as a cube
         GameObject corridor = GameObject.CreatePrimitive(PrimitiveType.Cube);
         corridor.transform.localScale = new Vector3(10f, 0.1f, distance);
         corridor.transform.position = midpoint;
 
-        // Rotate the corridor to face the direction of roomB
+        // Rotate the corridor to face the direction of destinationRoom
         Vector3 direction = destinationRoom.transform.position - startingRoom.transform.position;
         Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-        corridor.transform.rotation = rotation;
+        corridor.transform.rotation = rotation;*/
 
-        //GameObject corridor = Instantiate(corridorPrefab);
-        //corridor.transform.localScale = new Vector3(Mathf.Abs(roomA.transform.localScale.x - roomB.transform.localScale.x), 0, Mathf.Abs(roomA.transform.localScale.z - roomB.transform.localScale.z)   );
+        Vector3 midpointX = new Vector3 (((startingRoom.transform.position.x + destinationRoom.transform.position.x) / 2f), 0f, startingRoom.transform.position.z);
+        Vector3 midpointX2 = new Vector3(((startingRoom.transform.position.x + destinationRoom.transform.position.x) / 2f), 0f, destinationRoom.transform.position.z);
+        float deltaX = (destinationRoom.transform.position.x - startingRoom.transform.position.x);
+        Vector3 midpointY = new Vector3 (destinationRoom.transform.position.x, 0f, ((startingRoom.transform.position.z + destinationRoom.transform.position.z) / 2f));
+        Vector3 midpointY2 = new Vector3(startingRoom.transform.position.x, 0f, ((startingRoom.transform.position.z + destinationRoom.transform.position.z) / 2f));
+        float deltaY = (destinationRoom.transform.position.z - startingRoom.transform.position.z);
+        Vector3 connectPoint = new Vector3(destinationRoom.transform.position.x, 0f, startingRoom.transform.position.z);
+        Vector3 connectPoint2 = new Vector3(startingRoom.transform.position.x, 0f, destinationRoom.transform.position.z);
+
+        if ((destinationRoom.transform.position.x - startingRoom.transform.position.x) > (destinationRoom.transform.position.z - startingRoom.transform.position.z))
+        {
+            // Create a corridor as a cube
+            GameObject corridorX = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            corridorX.transform.localScale = new Vector3(10f, 0.1f, deltaX);
+            corridorX.transform.position = midpointX;
+
+            // Rotate the corridor to face the direction of destinationRoom
+            Vector3 directionX = new Vector3(destinationRoom.transform.position.x - startingRoom.transform.position.x, 0f, 0f);
+            Quaternion rotationX = Quaternion.LookRotation(directionX, Vector3.up);
+            corridorX.transform.rotation = rotationX;
+
+            GameObject corridorY = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            corridorY.transform.localScale = new Vector3(10f, 0.1f, deltaY);
+            corridorY.transform.position = midpointY;
+
+            // Rotate the corridor to face the direction of destinationRoom
+            Vector3 directionY = new Vector3(0f, 0f, destinationRoom.transform.position.z - startingRoom.transform.position.z);
+            Quaternion rotationY = Quaternion.LookRotation(directionY, Vector3.up);
+            corridorY.transform.rotation = rotationY;
+
+            GameObject corridorConnect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            corridorConnect.transform.localScale = new Vector3(10f, 0.1f, 10f);
+            corridorConnect.transform.position = connectPoint;
+        }
+        else if ((destinationRoom.transform.position.z - startingRoom.transform.position.z) > (destinationRoom.transform.position.x - startingRoom.transform.position.x))
+        {
+            GameObject corridorY = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            corridorY.transform.localScale = new Vector3(deltaY, 0.1f, 10f);
+            corridorY.transform.position = midpointY2;
+
+            // Rotate the corridor to face the direction of destinationRoom
+            Vector3 directionY = new Vector3(destinationRoom.transform.position.z - startingRoom.transform.position.z, 0f, 0f);
+            Quaternion rotationY = Quaternion.LookRotation(directionY, Vector3.up);
+            corridorY.transform.rotation = rotationY;
+
+            // Create a corridor as a cube
+            GameObject corridorX = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            corridorX.transform.localScale = new Vector3(deltaX, 0.1f, 10f);
+            corridorX.transform.position = midpointX2;
+
+            // Rotate the corridor to face the direction of destinationRoom
+            Vector3 directionX = new Vector3(0f, 0f, destinationRoom.transform.position.x - startingRoom.transform.position.x);
+            Quaternion rotationX = Quaternion.LookRotation(directionX, Vector3.up);
+            corridorX.transform.rotation = rotationX;
+
+            GameObject corridorConnect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            corridorConnect.transform.localScale = new Vector3(10f, 0.1f, 10f);
+            corridorConnect.transform.position = connectPoint2;
+        }
+
+        /*// Create a corridor as a cube
+        GameObject corridorX = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        corridorX.transform.localScale = new Vector3(10f, 0.1f, deltaX);
+        corridorX.transform.position = midpointX;
+
+        // Rotate the corridor to face the direction of destinationRoom
+        Vector3 directionX = new Vector3(destinationRoom.transform.position.x - startingRoom.transform.position.x, 0f, 0f);
+        Quaternion rotationX = Quaternion.LookRotation(directionX, Vector3.up);
+        corridorX.transform.rotation = rotationX;
+
+        GameObject corridorY = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        corridorY.transform.localScale = new Vector3(10f, 0.1f, deltaY);
+        corridorY.transform.position = midpointY;
+
+        // Rotate the corridor to face the direction of destinationRoom
+        Vector3 directionY = new Vector3(0f, 0f, destinationRoom.transform.position.z - startingRoom.transform.position.z);
+        Quaternion rotationY = Quaternion.LookRotation(directionY, Vector3.up);
+        corridorY.transform.rotation = rotationY;
+
+        GameObject corridorConnect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        corridorConnect.transform.localScale = new Vector3(10f, 0.1f, 10f);
+        corridorConnect.transform.position = connectPoint;*/
     }
 
     /*
