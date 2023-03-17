b13984b1cb452ea6517255c693b6777b1f6e800d
William Moses <wmoses5k@gmail.com>
2023-03-17 20:40:38 +0000

    Fixed DFS
    Instead of connecting currentRoom to the first room < maxDistance, it will connect to the closest room.

--- Dissertation/Assets/Scripts/RoomGenerator.cs
+++ Dissertation/Assets/Scripts/RoomGenerator.cs
@@ -54,38 +54,20 @@ public class RoomGenerator : MonoBehaviour
             rooms.Add(room);
         }
 
-        coroutine = WaitAndConnect(0.1f);
+        coroutine = WaitAndDFS(0.1f);
+        //coroutine = WaitAndConnectNearest(0.1f);
         StartCoroutine(coroutine);
     }
 
-    private IEnumerator WaitAndConnect(float waitTime)
+    private IEnumerator WaitAndDFS(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        DFS(rooms[0], visitedRooms, rooms, 100.0f);
+        DFS(rooms[0], visitedRooms, rooms, 1000.0f);
 
         for (int i = 0; i < visitedRooms.Count - 1; i++)
         {
-            ConnectRooms(visitedRooms[i], visitedRooms[i+1]);
+            ConnectRooms(visitedRooms[i], visitedRooms[i + 1]);
         }
-        /*ConnectRooms(rooms[0], rooms[1]);
-        ConnectRooms(rooms[1], rooms[2]);
-        ConnectRooms(rooms[2], rooms[3]);*/
-
-        /*ConnectRooms(visitedRooms[0], visitedRooms[1]);
-        ConnectRooms(visitedRooms[1], visitedRooms[2]);
-        ConnectRooms(visitedRooms[2], visitedRooms[3]);
-        ConnectRooms(visitedRooms[3], visitedRooms[4]);
-        ConnectRooms(visitedRooms[4], visitedRooms[5]);
-        ConnectRooms(visitedRooms[5], visitedRooms[6]);
-        ConnectRooms(visitedRooms[6], visitedRooms[7]);
-        ConnectRooms(visitedRooms[7], visitedRooms[8]);
-        ConnectRooms(visitedRooms[8], visitedRooms[9]);
-        ConnectRooms(visitedRooms[9], visitedRooms[10]);
-        ConnectRooms(visitedRooms[10], visitedRooms[11]);
-        ConnectRooms(visitedRooms[11], visitedRooms[12]);
-        ConnectRooms(visitedRooms[12], visitedRooms[13]);
-        ConnectRooms(visitedRooms[13], visitedRooms[14]);
-        ConnectRooms(visitedRooms[14], visitedRooms[15]);*/
     }
 
     public List<GameObject> FindAdjacentRooms(GameObject currentRoom, List<GameObject> roomsList, float maxDistance)
@@ -113,6 +95,8 @@ public class RoomGenerator : MonoBehaviour
         adjacentRooms = FindAdjacentRooms(currentRoom, rooms, maxDistance);
 
         visitedRooms.Add(currentRoom);
+
+        adjacentRooms.Sort((a, b) => Vector3.Distance(currentRoom.transform.position, a.transform.position).CompareTo(Vector3.Distance(currentRoom.transform.position, b.transform.position)));
 
         foreach (GameObject room in adjacentRooms)
         {
